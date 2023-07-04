37fe9efcfc8d817af7cbbb4c53c0d1665696ff9e
Disha Tamore <dishatamore2001@gmail.com>
2023-07-04 20:12:51 +0530

    Commit 1

--- WebPlacementManagementSystem/src/main/java/org/tnsif/project/placementmanagement/WebPlacementManagementSystemApplication.java
+++ WebPlacementManagementSystem/src/main/java/org/tnsif/project/placementmanagement/WebPlacementManagementSystemApplication.java
@@ -1,6 +1,7 @@
 package org.tnsif.project.placementmanagement;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
